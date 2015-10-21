c198eb628b93fef879338777dbf55051683d14fb
Richard Poore <mrrichpoore@gmail.com>
2015-10-21 09:55:16 -0500

    Initial Commit

---
+++ Copy of Git October/src/November.java
@@ -0,0 +1,11 @@
+
+public class November {
+
+	public static void main(String[] args) {
+		System.out.println("November is AWEsome");
+		System.out.println("My daughter was born then!");
+		System.out.println("And the weather is awesome in Texas");
+
+	}
+
+}
---
+++ Copy of Git October/src/October.java
@@ -0,0 +1,12 @@
+
+public class October {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("WooHoo! It's October");
+		System.out.println("And the weather is starting to get nice!");
+
+	}
+
+}
